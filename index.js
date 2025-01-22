8123f326644409434de965b667d0cc96c7f814c3
Dylan <dylan.kruger.za@gmail.com>
2025-01-22 13:18:13 +0200

    Added the required ternary operators to solve the problems

--- index.js
+++ index.js
@@ -5,14 +5,16 @@
 // print whether the user prefers a "Regular" or "Sugar-free" variety. If the preference is not set, default to "Regular".
 
 let userPreference = 'Sugar-free'; // Possible values: 'Regular', 'Sugar-free', or undefined
-console.log(``);
+console.log(userPreference || 'Regular');
 
 // #### Challenge 2: Stock Check
 // You have a variable that tracks the number of Monster Energy cans left in the fridge. 
 // Use a ternary operator to print "Time to restock!" if the number is less than 5, or "We're stocked!" if the number is 5 or more.
 
-let cansLeft = 3; // Any number of cans
-console.log(``);
+let cansLeft = 5; // Any number of cans
+console.log(cansLeft < 5 ? "Time to restock!" : "We're stocked!");
+
+
 
 
 // #### Challenge 3: Workout Intensity
@@ -20,7 +22,7 @@ console.log(``);
 // If the heart rate is below 100 bpm, print "Boost needed!", otherwise, print "Energy levels are high!".
 
 let heartRate = 95; // Current heart rate in bpm
-console.log(`'}`);
+console.log(heartRate < 100 ? "Boost Needed" : "Energy levels are high!");
 
 
 // #### Challenge 4: Temperature Suitability
@@ -28,7 +30,7 @@ console.log(`'}`);
 // if the temperature is below or equal to 5°C, or "Needs a cooler!" if above 5°C.
 
 let currentTemp = 4; // Current temperature in °C
-console.log(``);
+console.log(currentTemp < 5 ? "Chilled to perfection!" : "Needs a cooler!");
 
 
 // #### Challenge 5: Late Night Coding Session
@@ -42,6 +44,7 @@ let currentHour = 22;
 // Write your code below this line to determine if it's a good idea to have a Monster Energy drink based on the current hour
 // If it's between 7am and midnight (24-hour format), print "Unleash the beast!"
 // Otherwise, print "Better stick to water."
+console.log(currentHour >= 7 && currentHour < 24 ? "Unleash the beast!" : "Better stick to water!")
 
 
 
